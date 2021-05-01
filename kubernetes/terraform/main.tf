888812fa4ad262649f8613f8c38cfc0496733662
elena <ezhi92@mail.ru>
2021-05-01 22:04:42 +0500

    kubernetes-1

---
+++ kubernetes/terraform/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id = var.cloud_id
+  folder_id = var.folder_id
+  zone = var.zone
+}
+
+data "yandex_compute_image" "image" {
+  family = "ubuntu-1804-lts"
+}
+
+resource "yandex_compute_instance" "node" {
+  count = var.count_app * 3
+  name = "node-${count.index}"
+
+  resources {
+    cores = 4
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.image.id
+      size = 40
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat = true
+  }
+
+  metadata = {
+    user-data = file("files/metadata.yml")
+  }
+}
